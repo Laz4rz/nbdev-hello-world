4408692a39338d71b3f2db24b0b372a1e311d678
laz4rz <boronskimikolaj@gmail.com>
2022-08-25 16:56:24 +0200

    Add say_hello; add 01_whats_next; write some asserts

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -6,4 +6,6 @@ d = { 'settings': { 'branch': 'master',
                 'git_url': 'https://github.com/laz4rz/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
   'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.foo': ( 'https://laz4rz.github.io/core.html#foo',
-                                                                        'https://github.com/laz4rz/nbdev-hello-world/blob/master/nbdev_hello_world/core.py#L7')}}}
+                                                                        'https://github.com/laz4rz/nbdev-hello-world/blob/master/nbdev_hello_world/core.py#L7'),
+                                        'nbdev_hello_world.core.say_hello': ( 'https://laz4rz.github.io/core.html#say_hello',
+                                                                              'https://github.com/laz4rz/nbdev-hello-world/blob/master/nbdev_hello_world/core.py#L10')}}}
